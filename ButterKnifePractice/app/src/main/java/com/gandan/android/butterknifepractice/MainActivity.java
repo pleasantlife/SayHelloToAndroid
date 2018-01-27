771a620090b41b8a5cb25f6c0e4cbbd71d9fe9b1
pleasantlife <pleasantlife@gmail.com>
2018-01-27 22:59:12 +0900

    ButterKnife init

---
+++ ButterKnifePractice/app/src/main/java/com/gandan/android/butterknifepractice/MainActivity.java
@@ -0,0 +1,69 @@
+package com.gandan.android.butterknifepractice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //1. ButterKnife 사용법
+    //1) ButterKnife를 사용하기 위해 Annotation으로 BindView를 선언해준다.
+    //   (선언하면 Butterknife에서 자동으로 레이아웃에서 해당 id를 찾는다.)
+    //2) 괄호 안에 뷰의 id를 넣어준다.
+    //3) id에 바인딩할 데이터를 가진 타입과 변수를 선언한다.
+    @BindView(R.id.textView1)
+    TextView textView1;
+
+    @BindView(R.id.textView2)
+    TextView textView2;
+
+    @BindView(R.id.btnButton1)
+    Button btnButton1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //4) 버터나이프로 바인딩함을 선언해준다.
+        //findViewById를 선언할 수 있는 곳이면, 어디든 .bind()를 사용할 수 있다.
+        ButterKnife.bind(this);
+
+        /**ButterKnife를 사용하지 않으면?
+        * TextView noButtertextView1 = findViewById(R.id.textView1);
+        * TextView noButtertextView2 = findViewById(R.id.textView2);
+        * Button btnButton1 = findViewById(R.id.btnButton1);
+        * btnButton1.setOnClickListener(this);
+         */
+    }
+
+    //5) Button처럼 OnClickListener를 달아야 한다면, 아래와 같이 함수로 처리하면 된다.
+    //이 함수는 Annotation이 있어서, 다른 함수에 따로 선언하지 않아도 된다.
+    //단, private이나 static 함수로는 선언할 수 없다.
+    @OnClick(R.id.btnButton1)
+    public void btnChange(Button button){
+        button.setText("Hello!");
+        textView2.setText("Hello World!");
+    }
+
+    @Override
+    public void onClick(View v) {
+        /**
+         *  Butter Knife를 사용하지 않았을 때!
+
+         switch(v.getId()){
+            case R.id.btnButton1:
+                btnButton1.setText("Hello");
+                textView2.setText("Hello World!");
+            break;
+        }
+         */
+    }
+}
---
+++ ButterKnifePractice/readme.md
@@ -0,0 +1,51 @@
+# 버터나이프 배워보기
+
+## 1. 버터나이프란?
+    - 안드로이드 내에서 다양한 View를 사용할 떄 변수를 선언하고, 그 변수에 맞춰 View들을 연결하는 작업을 해야함.
+    - 버터나이프는 findViewById()로 대표되는 이러한 연결작업의 단계를 줄여줌.
+    - 사용하기도 쉬워서 코드의 간결성과 재사용성 확보.
+
+## 2. 버터나이프 설치
+
+    - Gradle(app)에 아래와 같이 입력한다.
+    <pre><code>
+    dependencies{
+        compile 'com.jakewharton:butterknife:8.8.1'
+        annotationProcessor 'com.jakewharton:butterknife-compiler:8.8.1'
+    }
+    </code></pre>
+
+## 3. 버터나이프 사용
+
+    - 버터나이프는 Annotation을 적극적으로 활용한다.
+    - 버터나이프로 바인딩하기
+    <pre><code>
+
+    public class MainActivity extends AppCompatActivity {   
+        
+        /** 1) ButterKnife를 사용하기 위해 Annotation으로 BindView를 선언해준다.
+        * (선언하면 Butterknife에서 자동으로 레이아웃에서 해당 id를 찾는다.)
+        * 괄호 안에 뷰의 id를 넣어준다.
+        * id에 바인딩할 데이터를 가진 타입과 변수를 선언한다.
+        */
+
+        @BindView(R.id.textView1) TextView textView1;
+        @BindView(R.id.textView2) TextView textView2;
+        @BindView(R.id.btnButton1) Button btnButton1;
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        /**4) 버터나이프로 바인딩함을 선언해준다.
+        * findViewById를 선언할 수 있는 곳이면, 어디든 .bind()를 사용할 수 있다.
+        */
+
+        ButterKnife.bind(this);
+
+    }
+
+    </code></pre>
