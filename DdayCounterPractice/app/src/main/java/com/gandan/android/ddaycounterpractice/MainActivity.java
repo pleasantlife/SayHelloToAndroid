f1bc0c7e779f82883ac649621462579015da2266
pleasantlife <pleasantlife@gmail.com>
2018-02-09 23:58:18 +0900

    180209 DdayCounterPractice Update.

--- DdayCounterPractice/app/src/main/java/com/gandan/android/ddaycounterpractice/MainActivity.java
+++ DdayCounterPractice/app/src/main/java/com/gandan/android/ddaycounterpractice/MainActivity.java
@@ -2,18 +2,34 @@ package com.gandan.android.ddaycounterpractice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText inputYear, inputMonth, inputDate;
+    long dDaylong;
+    Button btnCalculate;
+    TextView txtResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnCalculate = findViewById(R.id.btnCalculate);
+        inputYear = findViewById(R.id.inputYear);
+        inputMonth = findViewById(R.id.inputMonth);
+        inputDate = findViewById(R.id.inputDate);
+        txtResult = findViewById(R.id.txtResult);
+
+
 
 
         /*SimpleDateFormat dDayFormat = new SimpleDateFormat("yyyy년 MM월 DD일", Locale.KOREA);
@@ -35,5 +51,29 @@ public class MainActivity extends AppCompatActivity {
         } else if(dDayTime < 0) {
             txtItemDday.setText("D+" + (dDayTime * -1) + "일");
         }*/
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.btnCalculate:
+                String stringYear = inputYear.getText().toString();
+                String stringMonth = inputMonth.getText().toString();
+                String stringDate = inputDate.getText().toString();
+
+
+                SimpleDateFormat dDayFormat = new SimpleDateFormat("yyyy년 MM월 DD일", Locale.KOREA);
+
+                String dDayTargetString = stringYear+"년"+stringMonth+"월"+stringDate+"일";
+                String nowTime = dDayFormat.format(System.currentTimeMillis());
+                try {
+                    Date targetDate = dDayFormat.parse(dDayTargetString);
+                    Date nowDate = dDayFormat.parse(nowTime);
+                    dDaylong = (targetDate.getTime() - nowDate.getTime())/1000*60*60*24;
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                txtResult.setText("D-"+ dDaylong + "일");
+        }
     }
 }
