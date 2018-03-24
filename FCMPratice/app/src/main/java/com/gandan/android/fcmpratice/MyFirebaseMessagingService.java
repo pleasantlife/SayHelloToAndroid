0e51def30e2c0aa6e543a1796ac1520879b47130
pleasantlife <pleasantlife@gmail.com>
2018-03-24 23:23:08 +0900

    180324 FCMPractice update.

--- FCMPratice/app/src/main/java/com/gandan/android/fcmpratice/MyFirebaseMessagingService.java
+++ FCMPratice/app/src/main/java/com/gandan/android/fcmpratice/MyFirebaseMessagingService.java
@@ -1,6 +1,15 @@
 package com.gandan.android.fcmpratice;
 
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Icon;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -14,11 +23,25 @@ import com.google.firebase.messaging.RemoteMessage;
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
     String message;
 
+    NotificationChannel notificationChannel;
+
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
         message = remoteMessage.getNotification().getBody();
         Log.e("messageReceived", message+"");
+
+        if(Build.VERSION.SDK_INT >= 26){
+            Log.e("test", message+"");
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationChannel = new NotificationChannel(getString(R.string.fcm), "notiName", NotificationManager.IMPORTANCE_DEFAULT);
+            notificationChannel.setDescription("notification Test");
+            notificationManager.createNotificationChannel(notificationChannel);
+            Notification.Builder notiBuilder = new Notification.Builder(this, getString(R.string.fcm));
+            notiBuilder.setContentText(message).setSmallIcon(R.drawable.ic_launcher_foreground).setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher_background)).setChannelId(getString(R.string.fcm)).build();
+        } else {
+
+        }
     }
 
 }
