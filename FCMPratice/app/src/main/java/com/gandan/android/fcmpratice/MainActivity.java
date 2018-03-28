2d7d7d1e34fb89eb637e106ed1eb8ede98e8e1de
pleasantlife <pleasantlife@gmail.com>
2018-03-28 22:55:11 +0900

    180328 FCMPractice update.

--- FCMPratice/app/src/main/java/com/gandan/android/fcmpratice/MainActivity.java
+++ FCMPratice/app/src/main/java/com/gandan/android/fcmpratice/MainActivity.java
@@ -3,12 +3,16 @@ package com.gandan.android.fcmpratice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 
 import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.RemoteMessage;
 
 public class MainActivity extends AppCompatActivity {
 
     FirebaseInstanceId firebaseInstanceId;
+    Button btnSendNoti;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +21,11 @@ public class MainActivity extends AppCompatActivity {
         firebaseInstanceId = FirebaseInstanceId.getInstance();
         String token = firebaseInstanceId.getToken();
         Log.e("token" ,token+"");
+        btnSendNoti = findViewById(R.id.btnSendNoti);
+        btnSendNoti.setOnClickListener( v -> sendNoti());
     }
 
+    private void sendNoti(){
+    }
 
 }
--- FCMPratice/app/src/main/java/com/gandan/android/fcmpratice/MyFirebaseMessagingService.java
+++ FCMPratice/app/src/main/java/com/gandan/android/fcmpratice/MyFirebaseMessagingService.java
@@ -21,7 +21,7 @@ import com.google.firebase.messaging.RemoteMessage;
  */
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
-    String message;
+    String title, message;
     NotificationChannel notificationChannel;
     int id = 3;
 
@@ -32,7 +32,11 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         if(remoteMessage.getData().size() > 0){
             Log.e("remoteData", remoteMessage.getData()+"");
             Log.e("test", remoteMessage.getData().get("hello")+"");
-            message = remoteMessage.getData().get("hello");
+            title = remoteMessage.getData().get("title");
+            message = remoteMessage.getData().get("message");
+        } else {
+            title = remoteMessage.getNotification().getTitle();
+            message = remoteMessage.getNotification().getBody();
         }
         Log.e("messageReceived", message+"");
 
@@ -40,20 +44,24 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
             Log.e("test", message+"");
             NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-            notificationChannel = notificationManager.getNotificationChannel(getString(R.string.fcm));
+            notificationChannel = new NotificationChannel(getString(R.string.fcm), "Tested", NotificationManager.IMPORTANCE_DEFAULT);
+            //notificationChannel = notificationManager.getNotificationChannel(getString(R.string.fcm));
             notificationChannel.setDescription("notification Test");
             notificationManager.createNotificationChannel(notificationChannel);
-            Notification notiBuilder = new Notification.Builder(this).setContentTitle("Testing...").setContentText(message).setSmallIcon(R.drawable.ic_launcher_foreground).setChannelId(getString(R.string.fcm)).build();
+            //Notification notiBuilder = new Notification.Builder(this, getString(R.string.fcm)).setContentTitle("Testing...").setContentText(message).setSmallIcon(R.mipmap.ic_error_white_24dp).setChannelId(getString(R.string.fcm)).build();
+            NotificationCompat.Builder notificationCompat = new NotificationCompat.Builder(this, getString(R.string.fcm)).setContentTitle(title).setContentText(message).setSmallIcon(R.mipmap.ic_error_white_24dp);
             //아래의 int id는 정하기 나름~!
             //파이어베이스 콘솔에서 보내는 알림채널 id와 클라이언트 앱에서 받는 id가 같아야 온다! => 선택사항이라고 안쓰면 안된다.
-            notificationManager.notify(id, notiBuilder);
+            notificationManager.notify(id, notificationCompat.build());
+            onDeletedMessages();
 
         }
         //26버전 이하면 알림채널을 설정하지 않은 노티를 만들어줘도 잘 받음.
         else {
             NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-            NotificationCompat.Builder notificationCompat = new NotificationCompat.Builder(this).setContentTitle("Test Under 26").setContentText(message).setSmallIcon(R.mipmap.ic_launcher, 0);
+            NotificationCompat.Builder notificationCompat = new NotificationCompat.Builder(this, getString(R.string.fcm)).setContentTitle(title).setContentText(message).setSmallIcon(R.mipmap.ic_error_white_24dp);
             notificationManager.notify(id, notificationCompat.build());
+            onDeletedMessages();
         }
     }
 
