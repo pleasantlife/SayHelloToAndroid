79188b712962e0939ce280c4a08a4486fdc5fd56
pleasantlife <pleasantlife@gmail.com>
2018-06-03 14:19:39 +0900

    180603 KotlinFB update.

--- KotlinFB/app/src/main/java/com/gandan/android/kotlinfb/SplashActivity.kt
+++ KotlinFB/app/src/main/java/com/gandan/android/kotlinfb/SplashActivity.kt
@@ -3,24 +3,25 @@ package com.gandan.android.kotlinfb
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 
 class SplashActivity : AppCompatActivity() {
 
     private var firebaseAuth = FirebaseAuth.getInstance()
     private var firebaseUser = firebaseAuth.currentUser
+    //internal : 같은 모듈 내에서 모두 사용 가능한 변수.
+    internal lateinit var intent : Intent
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         Thread.sleep(2000)
-        if(firebaseUser === null){
-            var intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-            this.finish()
-        } else {
-            var intent = Intent(this, MainActivity::class.java)
+        firebaseUser.let {
+            intent = when(it) {
+                null -> Intent(this, LoginActivity::class.java)
+                else -> Intent(this, MainActivity::class.java)
+            }
             startActivity(intent)
             this.finish()
         }
