b796f0274ef103d02212a757a46322d90d99e18d
pleasantlife <pleasantlife@gmail.com>
2018-06-13 22:21:10 +0900

    180613 KotlinFB Update.(Firebase Storage)

--- KotlinFB/app/src/main/java/com/gandan/android/kotlinfb/WriteActivity.kt
+++ KotlinFB/app/src/main/java/com/gandan/android/kotlinfb/WriteActivity.kt
@@ -2,6 +2,7 @@ package com.gandan.android.kotlinfb
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -10,6 +11,8 @@ import android.widget.Toast
 import com.gandan.android.kotlinfb.adapter.FragmentWriteAdapter
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.activity_write.*
 import java.io.File
 
@@ -20,6 +23,9 @@ class WriteActivity : AppCompatActivity(), GetWriteDataListener {
     var firebaseDatabase = FirebaseDatabase.getInstance()
     var databaseReference = firebaseDatabase.reference
     var firebaseUser = FirebaseAuth.getInstance().currentUser
+    var firebaseStorage = FirebaseStorage.getInstance()
+    //괄호안에 넣은 String이 파일명이 된다!!
+    var storageReference = firebaseStorage.getReference("images")
 
     lateinit var complimentOne : String
     lateinit var complimentTwo : String
@@ -68,39 +74,46 @@ class WriteActivity : AppCompatActivity(), GetWriteDataListener {
     }
 
     override fun imageTwo(fileTwo: File) {
-        complimentImageOne = fileTwo
+        complimentImageTwo = fileTwo
     }
 
     override fun imageThree(fileThree: File) {
-        complimentImageOne = fileThree
+        complimentImageThree = fileThree
     }
 
     fun nullCheck(){
         when {
-            complimentOne == "" -> makeToast(0)
-            complimentTwo == "" -> makeToast(1)
-            complimentThree == "" -> makeToast(2)
+            complimentOne == "" -> makeToast(0, "내용")
+            complimentTwo == "" -> makeToast(1, "내용")
+            complimentThree == "" -> makeToast(2, "내용")
             //initlized 되었는지를 check하는 방법!!
-            !::complimentImageOne.isInitialized -> makeToast(0)
-            !::complimentImageTwo.isInitialized -> makeToast(1)
-            !::complimentImageThree.isInitialized -> makeToast(2)
-            else -> upload()
+            !::complimentImageOne.isInitialized -> makeToast(0, "사진")
+            !::complimentImageTwo.isInitialized -> makeToast(1, "사진")
+            !::complimentImageThree.isInitialized -> makeToast(2, "사진")
+            else -> uploadTexts()
         }
     }
 
-    fun makeToast(currentItem : Int){
-        Toast.makeText(this, "${currentItem+1}번 내용이 입력되지 않았습니다.", Toast.LENGTH_SHORT).show()
+    fun makeToast(currentItem : Int, type : String){
+        Toast.makeText(this, "${currentItem+1}번 ${type}이 입력되지 않았습니다.", Toast.LENGTH_SHORT).show()
         viewPagerWrite.currentItem = currentItem
     }
 
-    fun upload(){
+    fun uploadTexts(){
         databaseReference.ref.child(firebaseUser!!.uid).child("userdb").child("uploadDbTest").setValue(UploadWritten(complimentOne, complimentTwo, complimentThree)).addOnCompleteListener{
             if(it.isSuccessful) {
                 Toast.makeText(this, "업로드 완료!", Toast.LENGTH_SHORT).show()
+                uploadImages()
             }
         }.addOnFailureListener{ error ->
             Log.e("error when upload", error.message+"")
             Toast.makeText(this, "업로드 실패!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun uploadImages(){
+        storageReference.putFile(Uri.fromFile(complimentImageOne)).addOnCompleteListener{
+            task -> Log.e("url", task.result.downloadUrl.toString()+"")
         }
     }
 
