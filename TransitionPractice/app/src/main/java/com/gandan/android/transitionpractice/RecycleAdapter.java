090c607cd491590c19b3241ffa27a0be15687454
pleasantlife <pleasantlife@gmail.com>
2018-07-27 19:40:55 +0900

    180727 Transition Practice update.

--- TransitionPractice/app/src/main/java/com/gandan/android/transitionpractice/RecycleAdapter.java
+++ TransitionPractice/app/src/main/java/com/gandan/android/transitionpractice/RecycleAdapter.java
@@ -5,11 +5,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
+import android.support.transition.ChangeBounds;
+import android.support.transition.ChangeImageTransform;
+import android.support.transition.Transition;
+import android.support.transition.TransitionSet;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -72,6 +77,9 @@ public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.Holder> 
                     intent.putExtra("firstTextView", txtOne.getText().toString());
                     intent.putExtra("secondTextView", txtTwo.getText().toString());
                     ActivityOptionsCompat optionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation((Activity) context, imgItem,"image");
+                    android.transition.Transition transition = new android.transition.TransitionSet();
+                    transition.setDuration(600000);
+                    ((Activity) context).getWindow().setSharedElementExitTransition(transition);
                     context.startActivity(intent, optionsCompat.toBundle());
                 }
             });
