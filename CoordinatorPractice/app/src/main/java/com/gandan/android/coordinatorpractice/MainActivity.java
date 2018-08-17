65f5a4f1c3d1f1860ed5312893a73e98a0cbcb31
pleasantlife <pleasantlife@gmail.com>
2018-08-17 23:57:44 +0900

    180817 Coordinator Practice update.

--- CoordinatorPractice/app/src/main/java/com/gandan/android/coordinatorpractice/MainActivity.java
+++ CoordinatorPractice/app/src/main/java/com/gandan/android/coordinatorpractice/MainActivity.java
@@ -9,8 +9,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.Interpolator;
 
 import com.gandan.android.coordinatorpractice.adapter.TestViewPagerAdapter;
@@ -38,6 +40,17 @@ public class MainActivity extends AppCompatActivity implements ScrollListenerInt
 
         viewPager.setAdapter(testViewPagerAdapter);
         tabLayout.setupWithViewPager(viewPager, false);
+
+
+        for (int i = 0; i < 2; i++) {
+            TabLayout.Tab tab = tabLayout.getTabAt(i);
+            View tabView = ((ViewGroup) tabLayout.getChildAt(0)).getChildAt(i);
+            tabView.requestLayout();
+            View view = LayoutInflater.from(this).inflate(R.layout.tab_test, null);
+
+            tab.setCustomView(view);
+            //tab.setText(i + "");
+        }
 
 
     }
