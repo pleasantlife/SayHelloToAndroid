3d2e01e9dd6f361687bf682a732a8d469f6baa1a
pleasantlife <pleasantlife@gmail.com>
2019-02-07 23:21:43 +0900

    190207 DaumMap Practice update.

--- DaumMapPractice/app/src/main/java/com/gandan/android/daummappractice/MainActivity.java
+++ DaumMapPractice/app/src/main/java/com/gandan/android/daummappractice/MainActivity.java
@@ -1,24 +1,96 @@
 package com.gandan.android.daummappractice;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.widget.LinearLayout;
 
 import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class MainActivity extends AppCompatActivity {
 
-    ConstraintLayout daumMapView;
+    LinearLayout daumMapView;
+    MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MapView mapView = new MapView(this);
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo("com.gandan.android.daummappractice", PackageManager.GET_SIGNATURES);
+            for(Signature signature : info.signatures){
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.e("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+
         daumMapView = findViewById(R.id.daumMapView);
-        daumMapView.addView(mapView);
-        mapView.setMapCenterPoint(MapPoint.mapPointWithCONGCoord(37.497082, 126.028704), true);
+        mapView = findViewById(R.id.mapView);
+        mapView.setHDMapTileEnabled(true);
+        mapView.setMapType(MapView.MapType.Hybrid);
+        mapView.setShowCurrentLocationMarker(true);
+        mapView.setMapCenterPointAndZoomLevel(MapPoint.mapPointWithGeoCoord(37.4982086, 127.02776360000007), 1, true);
+        mapView.setMapViewEventListener(new MapView.MapViewEventListener() {
+            @Override
+            public void onMapViewInitialized(MapView mapView) {
+                Log.e("Init", mapView.getMapCenterPoint().getMapPointGeoCoord().latitude+"");
+            }
+
+            @Override
+            public void onMapViewCenterPointMoved(MapView mapView, MapPoint mapPoint) {
+
+            }
+
+            @Override
+            public void onMapViewZoomLevelChanged(MapView mapView, int i) {
+
+            }
+
+            @Override
+            public void onMapViewSingleTapped(MapView mapView, MapPoint mapPoint) {
+
+            }
+
+            @Override
+            public void onMapViewDoubleTapped(MapView mapView, MapPoint mapPoint) {
+
+            }
+
+            @Override
+            public void onMapViewLongPressed(MapView mapView, MapPoint mapPoint) {
+
+            }
+
+            @Override
+            public void onMapViewDragStarted(MapView mapView, MapPoint mapPoint) {
+
+            }
+
+            @Override
+            public void onMapViewDragEnded(MapView mapView, MapPoint mapPoint) {
+
+            }
+
+            @Override
+            public void onMapViewMoveFinished(MapView mapView, MapPoint mapPoint) {
+
+            }
+        });
     }
 }
