42edae1c33a04131d78ac1151c10c073cc2372e1
pleasantlife <pleasantlife@gmail.com>
2019-02-14 23:18:34 +0900

    190214 Drawing process (custom switch)

--- DrawingProcess/app/src/main/java/com/gandan/android/drawingprocess/MainActivity.java
+++ DrawingProcess/app/src/main/java/com/gandan/android/drawingprocess/MainActivity.java
@@ -4,11 +4,14 @@ import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView imageView4;
+    Switch switch1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +21,17 @@ public class MainActivity extends AppCompatActivity {
         imageView4 = findViewById(R.id.imageView4);
         //코드단에서 이미지의 색상 변경하기
         imageView4.getDrawable().setColorFilter(Color.parseColor("#EAEAEA"), PorterDuff.Mode.SRC_ATOP);
+
+        switch1 = findViewById(R.id.switch1);
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    switch1.getTrackDrawable().setColorFilter(Color.parseColor("#1ABC9C"), PorterDuff.Mode.SRC_IN);
+                } else {
+                    switch1.getTrackDrawable().setColorFilter(Color.parseColor("#EAEAEA"), PorterDuff.Mode.SRC_IN);
+                }
+            }
+        });
     }
 }
