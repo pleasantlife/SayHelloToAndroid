abd1d348a5671b47f393dbd29a004dd5af04be7c
pleasantlife <pleasantlife@gmail.com>
2019-02-28 23:26:21 +0900

    190228 Webview pdf practice update.

--- WebviewPdfPractice/app/src/main/java/com/gandan/android/webviewpdfpractice/MainActivity.java
+++ WebviewPdfPractice/app/src/main/java/com/gandan/android/webviewpdfpractice/MainActivity.java
@@ -1,10 +1,15 @@
 package com.gandan.android.webviewpdfpractice;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,8 +23,24 @@ public class MainActivity extends AppCompatActivity {
 
         webView = findViewById(R.id.webView);
 
-        webView.setWebViewClient(new WebViewClient());
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                Toast.makeText(MainActivity.this, "Load Finished!", Toast.LENGTH_SHORT);
+            }
+
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                super.onReceivedError(view, request, error);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    Log.e("receivedERR", error.getDescription().toString()+"");
+                }
+            }
+        });
+
         webView.setWebChromeClient(new WebChromeClient());
+
         webView.getSettings().setJavaScriptEnabled(true);
         if(fileUrl.contains(".pdf")){
             webView.loadUrl("https://docs.google.com/gview?embedded=true&url="+fileUrl);
